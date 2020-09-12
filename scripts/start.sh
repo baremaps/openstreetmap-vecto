fe3ecebd1ca7cb30635dde9e50393da469976ecc
Bertil Chapuis <bchapuis@gmail.com>
2020-09-12 21:06:29 +0200

    Fix port in the configuration

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,5 +3,5 @@
 baremaps serve \
   --database 'jdbc:postgresql://localhost:5432/osmvecto?allowMultiQueries=true&user=osmvecto&password=osmvecto' \
   --config 'config.yaml' \
-  --watch-changes
+  --watch-changes --log-level DEBUG
   
