2c1527bec68aca53325061caadd8b61a84f9b0ec
nicolas-heigvd <nicolas.blanc1@heig-vd.ch>
2021-02-25 23:38:22 +0100

    Update start script to take a config script as input arg. Take a default 'config.yml' if no arg is given.

--- start.sh
+++ start.sh
@@ -3,7 +3,13 @@
 HOST=${POSTGRES_HOST}
 PORT=${POSTGRES_PORT}
 
+if [ -z $1 ]; then
+    config_file='config.yml'
+else
+    config_file=$1
+fi
+
 baremaps preview \
   --database 'jdbc:postgresql://'${HOST}':'${PORT}'/'${POSTGRES_DB}'?allowMultiQueries=true&user='${POSTGRES_USER}'&password='${POSTGRES_PASSWORD} \
-  --config 'config.yml' \
+  --config "${config_file}" \
   --log-level=DEBUG
