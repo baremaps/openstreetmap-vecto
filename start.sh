020232d11892d67a3489e0f0f33db0072ded56ea
nicolas-heigvd <nicolas.blanc1@heig-vd.ch>
2021-02-25 21:31:01 +0100

    Add debug logs to start preview.

--- start.sh
+++ start.sh
@@ -5,4 +5,5 @@ PORT=${POSTGRES_PORT}
 
 baremaps preview \
   --database 'jdbc:postgresql://'${HOST}':'${PORT}'/'${POSTGRES_DB}'?allowMultiQueries=true&user='${POSTGRES_USER}'&password='${POSTGRES_PASSWORD} \
-  --config 'config.yaml'
+  --config 'config.yml' \
+  --log-level=DEBUG
