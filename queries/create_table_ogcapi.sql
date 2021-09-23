a1a4b265f5ea1bbe88e48d2754588abc90bd9475
Balthasar Teuscher <balthasar.teuscher@gmail.com>
2021-09-23 12:25:03 +0200

    Update collection schema

--- queries/create_table_ogcapi.sql
+++ queries/create_table_ogcapi.sql
@@ -1,23 +1,14 @@
-create table if not exists collections (
-    id uuid primary key,
-    title text,
-    description text,
-    links jsonb[],
-    extent jsonb,
-    item_type text default 'feature',
-    crs text[]
+CREATE TABLE IF NOT EXISTS collections (
+    id uuid PRIMARY KEY,
+    collection jsonb
 );
 
-
-CREATE TABLE IF NOT EXISTS styles
-(
-    id       uuid PRIMARY KEY,
-    style    jsonb
+CREATE TABLE IF NOT EXISTS styles (
+    id uuid PRIMARY KEY,
+    style jsonb
 );
 
-CREATE TABLE IF NOT EXISTS tilesets
-(
-    id         uuid PRIMARY KEY,
-    tileset    jsonb
+CREATE TABLE IF NOT EXISTS tilesets (
+    id uuid PRIMARY KEY,
+    tileset jsonb
 );
-
