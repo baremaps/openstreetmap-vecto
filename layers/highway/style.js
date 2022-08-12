b366e732fa1e28417aa6fb6050fa074c0380b700
Bertil Chapuis <bchapuis@gmail.com>
2022-08-12 09:23:46 +0200

    Change naming convension for highways

--- layers/highway/style.js
+++ layers/highway/style.js
@@ -1,14 +1,13 @@
-import layer from '../layer.js'
-
-import highway_line from './highway_line.js'
-import highway_outline from './highway_outline.js'
-import highway_dash from './highway_dash.js'
-import tunnel_line from './tunnel_line.js'
-import tunnel_outline from './tunnel_outline.js'
-import pedestrian_area from './pedestrian_area.js'
-import highway_label from './highway_label.js'
-import bridge_line from './bridge_line.js'
-import bridge_outline from './bridge_outline.js'
+import layer from '../layer.js';
+import style_highway_line from './style_highway_line.js';
+import style_highway_outline from './style_highway_outline.js';
+import style_highway_dash from './style_highway_dash.js';
+import style_tunnel_line from './style_tunnel_line.js';
+import style_tunnel_outline from './style_tunnel_outline.js';
+import style_pedestrian_area from './style_pedestrian_area.js';
+import style_highway_label from './style_highway_label.js';
+import style_bridge_line from './style_bridge_line.js';
+import style_bridge_outline from './style_bridge_outline.js';
 
 // TODO:
 // recover construction styles.
@@ -16,14 +15,13 @@ import bridge_outline from './bridge_outline.js'
 // https://wiki.openstreetmap.org/wiki/Key:construction
 
 export default [
-
-    ...layer(tunnel_outline),
-    ...layer(tunnel_line),
-    ...layer(highway_outline),
-    ...layer(highway_line),
-    ...layer(highway_dash),
-    ...layer(pedestrian_area),
-    ...layer(bridge_line),
-    ...layer(bridge_outline),
-    ...layer(highway_label),
+    ...layer(style_tunnel_outline),
+    ...layer(style_tunnel_line),
+    ...layer(style_highway_outline),
+    ...layer(style_highway_line),
+    ...layer(style_highway_dash),
+    ...layer(style_pedestrian_area),
+    ...layer(style_bridge_line),
+    ...layer(style_bridge_outline),
+    ...layer(style_highway_label),
 ];
